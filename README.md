641b24ada3710b6d2311022743b47433cf9290da
ipotqo <125836089+ipotqo@users.noreply.github.com>
2023-02-22 03:09:04 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)
+![cmake](https://github.com/ipotqo/miscellanea/actions/workflows/cmake.yml/badge.svg)
+
 # miscellanea
-Various Tidbits
+Various Tidbits and Trivia of the Useful and/or Useless Kind
--- src/erasure.cpp
+++ src/erasure.cpp
@@ -5,7 +5,59 @@
 
 #include <iostream>
 
+/**
+ * Feature Wrapper
+ */
 class feature {
+
+private:
+
+    // "Concept"
+    struct FeatureNotion {
+
+        virtual ~FeatureNotion () = default;
+
+        virtual void serialisation () const = 0;
+        virtual void visualisation () const = 0;
+
+    };
+
+    template< typename FeatureT >
+    struct FeatureModel : public FeatureNotion {
+
+        FeatureModel ( FeatureT i_feature )
+            : m_feature { std::move( i_feature )  }
+        {}
+
+        void serialisation () const override {
+            serialise ( m_feature );
+        }
+
+        void visualisation () const override {
+            visualise ( m_feature );
+        }
+
+        FeatureT m_feature;
+
+    };
+
+    // pimpl idiom
+    std::unique_ptr<FeatureNotion> pimpl;
+
+public:
+
+    template< typename FeatureT >
+    feature ( FeatureT i_feature )
+        : pimpl { std::make_unique<FeatureModel<FeatureT>>( std::move(i_feature) ) }
+    {}
+
+    // Copy Operations
+    feature ( feature const& other );
+    feature& operator= ( feature const& other );
+
+    // Move Operations
+    feature ( feature&& other );
+    feature& operator= ( feature&& other );
 
 };
 
